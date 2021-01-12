5b0b32d3300fbfe6051d588d91add9caff605f44
Nicolas Bouvard <pro.nicolas.bouvard@gmail.com>
2021-01-12 19:12:07 +0100

    Adding smooth camera over player

---
+++ Assets/Scripts/UI/SmoothCamera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = Vector3.zero;
+    public float smoothness = 0.15f;
+    
+    private Vector3 velocity = Vector3.zero;
+    
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothness);
+    }
+}
