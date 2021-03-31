d535bd902795235bceb7790426def9acadda998c
Nicolas Bouvard <pro.nicolas.bouvard@gmail.com>
2021-03-31 15:37:05 +0200

    Fix merge errors on test scene

--- Assets/Scripts/UI/SwagBar.cs
+++ Assets/Scripts/UI/SwagBar.cs
@@ -7,20 +7,21 @@ public class SwagBar : MonoBehaviour
 {
     private PlayerStateManager _playerState;
 
-    public GameObject _swagBar;
+    private Slider _swagBar;
     // Start is called before the first frame update
     void Start()
     {
         _playerState = GameObject.FindWithTag("Player").GetComponent<PlayerStateManager>();
+        _swagBar = GetComponent<Slider>();
         Debug.Log($"_playerState: {_playerState == null} - _swagBar: {_swagBar == null}");
-        _swagBar.GetComponent<Slider>().maxValue = _playerState.swagMax;
-        // _swagBar.maxValue = _playerState.swagMax;
+        // _swagBar.GetComponent<Slider>().maxValue = _playerState.swagMax;
+        _swagBar.maxValue = _playerState.swagMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _swagBar.GetComponent<Slider>().value = _playerState.GetSwagValue();
-        // _swagBar.value = _playerState.GetSwagValue();
+        // _swagBar.GetComponent<Slider>().value = _playerState.GetSwagValue();
+        _swagBar.value = _playerState.GetSwagValue();
     }
 }
